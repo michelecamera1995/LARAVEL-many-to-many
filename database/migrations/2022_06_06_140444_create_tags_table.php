3ced68ef6f57d4031e26e5ae3aa8574c0ba23388
michele camera <michy.cam95@gmail.com>
2022-06-06 16:06:59 +0200

    created migrations

--- database/migrations/2022_06_06_140444_create_tags_table.php
+++ database/migrations/2022_06_06_140444_create_tags_table.php
@@ -14,9 +14,12 @@ class CreateTagsTable extends Migration
     public function up()
     {
         Schema::create('tags', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->timestamps();
+            Schema::table('tags', function (Blueprint $table) {
+                //
+                $table->id();
+                $table->string('name');
+                $table->timestamps();
+            });
         });
     }
 
---
+++ database/migrations/2022_06_06_140512_create_post_tags_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('post_tags', function (Blueprint $table) {
+            $table->unsignedBigInteger('post_id');
+            $table->foreign('post_id')
+                ->references('id')
+                ->on('posts');
+
+
+            $table->unsignedBigInteger('tags_id');
+            $table->foreign('tags_id')
+                ->references('id')
+                ->on('tags');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_tags');
+    }
+}
