46d3ace345b4fed72da91b72c498030e1cc65e00
michele camera <michy.cam95@gmail.com>
2022-06-07 09:55:44 +0200

    update show and index

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,7 +1,8 @@
 <div>
     <a class="nav-link" href="{{ route('admin.posts.create') }}">Create new post</a>
     @foreach($posts as $post)
-    <h2>Titolo: {{ $post->title}}</h2>
+    <h2>POSTS</h2>
+    <h2>Title: {{ $post->title}}</h2>
     <button><a class="nav-link" href="{{ route('admin.posts.edit', $post) }}">Modify</a></button>
     <button><a class="nav-link" href="{{ route('admin.posts.show', $post) }}">Details</button>
     <form action="{{route('admin.posts.destroy', $post)}}" method="POST">
--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -1,5 +1,9 @@
 <div>
-    <h2>Contenuto:</h2>
+    <h2>Details:</h2>
+    <h3>Title:</h3>
+    <p>{{ $post->title}}</p>
+    <h3>Content:</h3>
     <p>{{ $post->content}}</p>
+    <h3>Category:</h3>
     <p>{{ $categories->name}}</p>
 </div>
